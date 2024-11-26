866984a3a3c16812bfd13607c209fa3bd18e0a21
Giovanni Neves <giovanni_neves@protonmail.com>
2024-11-26 20:40:21 -0300

    Feat: Add reusable Button component with customizable properties

---
+++ resources/js/Components/Button.vue
@@ -0,0 +1,50 @@
+<script setup>
+import { ref } from "vue";
+
+defineProps({
+    type: {
+        type: String,
+        default: "button",
+    },
+    color: {
+        type: String,
+        default: "primary",
+    },
+    size: {
+        type: String,
+        default: "md",
+    },
+    disabled: {
+        type: Boolean,
+        default: false,
+    },
+});
+
+const btnColor = ref({
+    primary: "btn-primary",
+    secondary: "btn-secondary",
+    neutral: "btn-neutral",
+    accent: "btn-accent",
+    ghost: "btn-ghost",
+    link: "btn-link",
+    error: "btn-error",
+    info: "btn-info",
+    success: "btn-success",
+    warning: "btn-warning",
+});
+
+const btnSize = ref({
+    xs: "btn-xs",
+    sm: "btn-sm",
+    md: "",
+    lg: "btn-lg",
+});
+
+
+</script>
+
+<template>
+    <button :type="type" :class="`btn ${btnColor[color]} ${btnSize[size]} mx-1`" @click="handleClick" :disabled="disabled">
+        <slot></slot>
+    </button>
+</template>

f53bf1f48a4954594408cd213a4dd833479831e9
Giovanni Neves <giovanni_neves@protonmail.com>
2024-11-28 07:37:00 -0300

    Feat: Add Pen and TrashBin icon components with customizable properties

---
+++ resources/js/Components/Icons/Pen.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+    width: {
+        type: String,
+        default: "16",
+    },
+    height: {
+        type: String,
+        default: "16",
+    },
+    color: {
+        type: String,
+        default: "#000000",
+    },
+});
+</script>
+
+<template>
+    <svg
+        :width="width"
+        :height="height"
+        viewBox="0 0 16 16"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+            <path
+                d="M8.29289 3.70711L1 11V15H5L12.2929 7.70711L8.29289 3.70711Z"
+                :fill="color"
+            ></path>
+            <path
+                d="M9.70711 2.29289L13.7071 6.29289L15.1716 4.82843C15.702 4.29799 16 3.57857 16 2.82843C16 1.26633 14.7337 0 13.1716 0C12.4214 0 11.702 0.297995 11.1716 0.828428L9.70711 2.29289Z"
+                :fill="color"
+            ></path>
+        </g>
+    </svg>
+</template>
---
+++ resources/js/Components/Icons/TrashBin.vue
@@ -0,0 +1,41 @@
+<script setup>
+defineProps({
+    width: {
+        type: String,
+        default: "16",
+    },
+    height: {
+        type: String,
+        default: "16",
+    },
+    color: {
+        type: String,
+        default: "#000000",
+    },
+});
+</script>
+
+<template>
+    <svg
+        :width="width"
+        :height="height"
+        viewBox="0 0 16 16"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+            <path d="M4 2H1V4H15V2H12V0H4V2Z" :fill="color"></path>
+            <path
+                fill-rule="evenodd"
+                clip-rule="evenodd"
+                d="M3 6H13V16H3V6ZM7 9H9V13H7V9Z"
+                :fill="color"
+            ></path>
+        </g>
+    </svg>
+</template>

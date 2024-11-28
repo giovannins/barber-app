072f1e5d1988ba855a72695c64868768d7798022
Giovanni Neves <giovanni_neves@protonmail.com>
2024-11-28 07:40:20 -0300

    Feat: Add LinesMagnifying icon component with customizable size and color

---
+++ resources/js/Components/Icons/LinesMagnifying.vue
@@ -0,0 +1,48 @@
+<script setup>
+defineProps({
+    size: {
+        type: String,
+        default: "16",
+    },
+    color: {
+        type: String,
+        default: "#000000",
+    },
+});
+</script>
+
+<template>
+    <svg
+        :width="size"
+        :height="size"
+        viewBox="0 0 16 16"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+    >
+        <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+        <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+        ></g>
+        <g id="SVGRepo_iconCarrier">
+            <path d="M15 1H1V3H15V1Z" :fill="color"></path>
+            <path
+                d="M11 5H1V7H6.52779C7.62643 5.7725 9.223 5 11 5Z"
+                :fill="color"
+            ></path>
+            <path
+                d="M5.34141 13C5.60482 13.7452 6.01127 14.4229 6.52779 15H1V13H5.34141Z"
+                :fill="color"
+            ></path>
+            <path
+                d="M5.34141 9C5.12031 9.62556 5 10.2987 5 11H1V9H5.34141Z"
+                :fill="color"
+            ></path>
+            <path
+                d="M15 11C15 11.7418 14.7981 12.4365 14.4462 13.032L15.9571 14.5429L14.5429 15.9571L13.032 14.4462C12.4365 14.7981 11.7418 15 11 15C8.79086 15 7 13.2091 7 11C7 8.79086 8.79086 7 11 7C13.2091 7 15 8.79086 15 11Z"
+                :fill="color"
+            ></path>
+        </g>
+    </svg>
+</template>

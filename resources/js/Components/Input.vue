62bbdc894a29136361fa713d0e9d82361c8453c7
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-01 13:59:58 -0300

    Feat: Create Input component with props for id, label, placeholder, type, icon, and modelValue

---
+++ resources/js/Components/Input.vue
@@ -0,0 +1,52 @@
+<script setup>
+import { useAttrs } from 'vue';
+
+const props = defineProps({
+    id: {
+        type: String,
+        required: true,
+    },
+    label: {
+        type: String,
+        required: true,
+    },
+    placeholder: {
+        type: String,
+        default: 'Type here',
+    },
+    type: {
+        type: String,
+        default: 'text',
+    },
+    icon: {
+        type: String,
+        default: '',
+    },
+    modelValue: {
+        default: '',
+    },
+});
+
+const emit = defineEmits(['update:modelValue']);
+
+const updateValue = (event) => {
+    emit('update:modelValue', event.target.value);
+};
+
+</script>
+
+<template>
+    <label :for="id" class="form-controll w-full max-w-xs">
+        <div class="label">
+            <span class="label-text">{{ label }}</span>
+        </div>
+        <input
+            :id="id"
+            :type="type"
+            :placeholder="placeholder"
+            :value="modelValue"
+            @input="updateValue"
+            class="input input-bordered w-full max-w-xs"
+        />
+    </label>
+</template>

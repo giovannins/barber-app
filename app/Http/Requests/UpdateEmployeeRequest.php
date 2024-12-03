fca9dcdc7a0a045c7fecb3ec3f1c369ae4f252b5
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-03 19:40:03 -0300

    Update: Authorization logic and validation rules in UpdateEmployeeRequest

--- app/Http/Requests/UpdateEmployeeRequest.php
+++ app/Http/Requests/UpdateEmployeeRequest.php
@@ -11,7 +11,7 @@ class UpdateEmployeeRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,10 @@ class UpdateEmployeeRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:255',
+            'phone' => 'required|string|max:15',
+            'speciality' => 'required|string|max:255',
+            'hire_date' => 'required|date',
         ];
     }
 }

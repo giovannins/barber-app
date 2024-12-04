ca61de4bb1a401ec7961543bd59573724b0cc2e6
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-03 21:05:09 -0300

    Feat: Add role column to users table and seed super_admin role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -23,6 +23,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
             'password' => bcrypt('password'),
+            'role' => 'super_admin',
         ]);
 
         Barbershop::factory()->createMany($records);

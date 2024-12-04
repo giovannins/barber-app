4637feb2e4c4a1be88898fa0f8c3fd1f3f9bb6c3
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-04 07:38:46 -0300

    Feat: Add EnsureSuperAdmin middleware for super admin access control and update routes

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\BarbershopController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Middleware\EnsureSuperAdmin;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -24,18 +25,19 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
 
+Route::middleware(['auth', EnsureSuperAdmin::class])->group(function () {
+    Route::get('/dashboard/super_admin', [DashboardController::class, 'index'])->name('super_admin.dashboard');
 
-Route::get('/dashboard/super_admin/barbershops', [BarbershopController::class,'index'])->name('super_admin.barbershops.index');
-Route::get('/dashboard/super_admin/barbershops/{barbershop}', [BarbershopController::class,'show'])->name('super_admin.barbershops.show');
-Route::get('/dashboard/super_admin/barbershops/{barbershop}/edit', [BarbershopController::class,'edit'])->name('super_admin.barbershops.edit');
-Route::put('/dashboard/super_admin/barbershops/{barbershop}/update', [BarbershopController::class,'update'])->name('super_admin.barbershops.update');
+    Route::get('/dashboard/super_admin/barbershops', [BarbershopController::class, 'index'])->name('super_admin.barbershops.index');
+    Route::get('/dashboard/super_admin/barbershops/{barbershop}', [BarbershopController::class, 'show'])->name('super_admin.barbershops.show');
+    Route::get('/dashboard/super_admin/barbershops/{barbershop}/edit', [BarbershopController::class, 'edit'])->name('super_admin.barbershops.edit');
+    Route::put('/dashboard/super_admin/barbershops/{barbershop}/update', [BarbershopController::class, 'update'])->name('super_admin.barbershops.update');
 
-Route::get('/dashboard/super_admin/employees', [EmployeeController::class,'index'])->name('super_admin.employees.index');
-Route::get('/dashboard/super_admin/employees/{employee}', [EmployeeController::class,'show'])->name('super_admin.employees.show');
-Route::get('/dashboard/super_admin/employees/{employee}/edit', [EmployeeController::class,'edit'])->name('super_admin.employees.edit');
-Route::put('/dashboard/super_admin/employees/{employee}/update', [EmployeeController::class,'update'])->name('super_admin.employees.update');
-Route::delete('/dashboard/super_admin/employees/{employee}/delete', [EmployeeController::class,'destroy'])->name('super_admin.employees.delete');
-
-Route::get('/dashboard/super_admin', [DashboardController::class, 'index'])->name('super_admin.dashboard');
+    Route::get('/dashboard/super_admin/employees', [EmployeeController::class, 'index'])->name('super_admin.employees.index');
+    Route::get('/dashboard/super_admin/employees/{employee}', [EmployeeController::class, 'show'])->name('super_admin.employees.show');
+    Route::get('/dashboard/super_admin/employees/{employee}/edit', [EmployeeController::class, 'edit'])->name('super_admin.employees.edit');
+    Route::put('/dashboard/super_admin/employees/{employee}/update', [EmployeeController::class, 'update'])->name('super_admin.employees.update');
+    Route::delete('/dashboard/super_admin/employees/{employee}/delete', [EmployeeController::class, 'destroy'])->name('super_admin.employees.delete');
+});

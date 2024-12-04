14a2737cea7613c9f18ed5a3772236c034207c32
Giovanni Neves <giovanni_neves@protonmail.com>
2024-12-04 07:39:00 -0300

    Refactor: Remove Navbar component and integrate its functionality directly into SuperAdminLayout

--- resources/js/Layouts/SuperAdminLayout.vue
+++ resources/js/Layouts/SuperAdminLayout.vue
@@ -1,10 +1,44 @@
 <script setup>
-import Navbar from '@/Components/Navbar.vue';
-
+import { Link } from "@inertiajs/vue3";
 </script>
 
 <template>
-    <Navbar/>
+    <nav class="navbar bg-neutral text-neutral-content md:visible invisible">
+        <div class="flex-1">
+            <a class="btn btn-primary text-xl"></a>
+        </div>
+        <div class="flex-none">
+            <ul class="menu menu-horizontal px-1">
+                <li>
+                    <Link :href="route('super_admin.dashboard')"
+                        >Dashboard</Link
+                    >
+                </li>
+                <li>
+                    <details>
+                        <summary>Profile</summary>
+                        <ul
+                            class="bg-neutral text-neutral-content rounded-t-none z-[1] p-2"
+                        >
+                            <li>
+                                <Link :href="route('profile.edit')"
+                                    >Edit Profile</Link
+                                >
+                            </li>
+                            <li>
+                                <Link
+                                    :href="route('logout')"
+                                    method="post"
+                                    as="button"
+                                    >Log Out</Link
+                                >
+                            </li>
+                        </ul>
+                    </details>
+                </li>
+            </ul>
+        </div>
+    </nav>
 
     <main>
         <slot />
